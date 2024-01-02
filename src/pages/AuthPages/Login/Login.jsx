7c1c20e03d2539fd60fea62373ecec9ebfb611b7
Sanyam Jain <sanyamjain6667@gmail.com>
2024-01-02 17:24:01 +0530

    indentation solved

--- src/pages/AuthPages/Login/Login.jsx
+++ src/pages/AuthPages/Login/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginForm from '../../../components/AuthComponents/LoginForm';
 import './Login.css'; // Import your styles
-
 const LoginPage = () => {
   return (
     <div className='login-container'>
@@ -11,10 +10,23 @@ const LoginPage = () => {
 
         <div className=''>
           <LoginForm />
-          <Link to="/register" className='login-register-link'>Not a member? Register</Link>
         </div>
+        <h1 className='heading-2'>Or</h1>
+        <form autoComplete='off'>
+         
+          <button type='button'  className='google-signin-btn'>
+            <span className='google-icon'>
+    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+     alt="Google Logo"/>
+            </span>
+            Sign in with Google
+          </button>
+          <Link to="/register" className='login-register-link'>Not a member? Register</Link>
+        </form>
+       
       </div>
     </div>
+
   );
 };
 
