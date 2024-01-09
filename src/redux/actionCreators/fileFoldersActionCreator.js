d3c5b43cf608ed9534ab0577efa0109b711e1b3c
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2024-01-09 18:26:16 +0530

    Update fileFoldersActionCreator.js

--- src/redux/actionCreators/fileFoldersActionCreator.js
+++ src/redux/actionCreators/fileFoldersActionCreator.js
@@ -175,7 +175,12 @@ export const uploadFile =(file,data,setSuccess) =>(dispatch) => {
         uploadFileRef.put(file).on('state_changed', (snapshot) => {
 
             const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-         toast.success("uploading " + progress + "% done");
+          if(progress === 100) {
+                toast.success("uploading " + progress + "% done");
+            }
+            else{
+                toast.error("uploading " + progress + "% done");
+            }
 
 
 }, (err) => {
